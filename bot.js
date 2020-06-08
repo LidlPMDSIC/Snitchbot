2ff8628ae308356a2a080db58c4eb242c86828c7
LidlPMDSIC <60274080+LidlPMDSIC@users.noreply.github.com>
2020-06-08 13:35:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,23 +3,37 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
  
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+const commands = new discord.command.CommandGroup({
+  defaultPrefix: 'gc ' // You can customize your default prefix here.
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+commands.raw('snitchrate', async (message) => {
+  // We're going to fetch a fact from the cat-fact API.
+  var n = Math.floor(Math.random() * 101);
+  if (n == 0) await message.reply(`Sei snitch al ${n}% Sei DIO`);
+  else {
+    if (n <= 20)
+      await message.reply(`Sei snitch al ${n}% Bravo, posso fidarmi di te`);
+    else {
+      if (n < 50)
+        await message.reply(`Sei snitch al ${n}% Sei sulla buona strada`);
+      else {
+        if (n == 100)
+          await message.reply(`Sei snitch al ${n}% Peggio di brumotti`);
+        else {
+          if (n >= 80)
+            await message.reply(`Sei snitch al ${n}% Non ti fai schifo?`);
+          else {
+            if (n > 50)
+              await message.reply(
+                `Sei snitch al ${n}% Sei sulla cattiva strada`
+              );
+            else await message.reply(`Sei snitch al ${n}% Normie`);
+          }
+        }
+      }
+    }
+  }
 });
 
  
